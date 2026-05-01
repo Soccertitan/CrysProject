0b145b2dc00d1c6632f768821adaa7911253e3d5
Soccertitan <tristan.nowacki@outlook.com>
2026-05-01 14:46:28 -0700

    Initial commit

---
+++ Source/CrysProject/CrysProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CrysProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrysProject, "CrysProject" );
