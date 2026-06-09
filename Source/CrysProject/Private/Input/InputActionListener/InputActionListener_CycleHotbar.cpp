31bb4f9d053467e89bc862803bc5f9109e6d1009
Soccertitan <tristan.nowacki@outlook.com>
2026-06-09 16:11:49 -0700

    InputAction Hotbar
    Added more functionality for hotbar inputs.

---
+++ Source/CrysProject/Private/Input/InputActionListener/InputActionListener_CycleHotbar.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright Soccertitan 2026
+
+
+#include "Input/InputActionListener/InputActionListener_CycleHotbar.h"
+
+#include "InputActionValue.h"
+#include "ActionSystem/CrysActionManagerComponent.h"
+
+void UInputActionListener_CycleHotbar::InputActionTriggered(const FInputActionValue& Value)
+{
+	Super::InputActionTriggered(Value);
+	
+	if (ActionManagerComponent)
+	{
+		const bool bCycleForward = Value.GetMagnitude() > 0.f;
+		ActionManagerComponent->SwitchToNextActionSet(bCycleForward);
+	}
+}
+
+void UInputActionListener_CycleHotbar::Initialize()
+{
+	Super::Initialize();
+	
+	ActionManagerComponent = GetPlayerController()->FindComponentByClass<UCrysActionManagerComponent>();
+}
---
+++ Source/CrysProject/Public/Input/InputActionListener/InputActionListener_CycleHotbar.h
@@ -0,0 +1,28 @@
+﻿// Copyright Soccertitan 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CrysInputActionListener.h"
+#include "InputActionListener_CycleHotbar.generated.h"
+
+class UCrysActionManagerComponent;
+
+/**
+ * Switches a hotbar to the next or previous one. Cycles through.
+ */
+UCLASS()
+class CRYSPROJECT_API UInputActionListener_CycleHotbar : public UCrysInputActionListener
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void InputActionTriggered(const FInputActionValue& Value) override;
+	
+protected:
+	virtual void Initialize() override;
+	
+private:
+	UPROPERTY()
+	TObjectPtr<UCrysActionManagerComponent> ActionManagerComponent;
+};
