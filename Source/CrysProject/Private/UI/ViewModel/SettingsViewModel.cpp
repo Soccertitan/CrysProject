06667eef6eec895155502983098e9b3192efb659
Soccertitan <tristan.nowacki@outlook.com>
2026-06-18 13:54:30 -0700

    Created a basic Settings ViewModel
    The settings VM just updates the ability input mode.

---
+++ Source/CrysProject/Private/UI/ViewModel/SettingsViewModel.cpp
@@ -0,0 +1,31 @@
+﻿// Copyright Soccertitan 2026
+
+
+#include "UI/ViewModel/SettingsViewModel.h"
+
+#include "Player/HeroPlayerController.h"
+
+void USettingsViewModel::InitializeViewModel(APlayerController* PlayerController)
+{
+	Super::InitializeViewModel(PlayerController);
+	OwnerPC = PlayerController;
+	
+	if (AHeroPlayerController* HeroPC = Cast<AHeroPlayerController>(PlayerController))
+	{
+		SetAbilityInputMode(HeroPC->GetAbilityInputMode());
+		HeroPC->OnAbilityInputModeChangedDelegate.AddUniqueDynamic(this, &USettingsViewModel::SetAbilityInputMode);
+	}
+}
+
+void USettingsViewModel::K2_SetAbilityInputMode(const EAbilityInputMode NewValue)
+{
+	if (AHeroPlayerController* HeroPC = Cast<AHeroPlayerController>(OwnerPC))
+	{
+		HeroPC->SetAbilityInputMode(NewValue);
+	}
+}
+
+void USettingsViewModel::SetAbilityInputMode(const EAbilityInputMode NewValue)
+{
+	UE_MVVM_SET_PROPERTY_VALUE(AbilityInputMode, NewValue);
+}
---
+++ Source/CrysProject/Public/UI/ViewModel/SettingsViewModel.h
@@ -0,0 +1,36 @@
+﻿// Copyright Soccertitan 2026
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CrysViewModel.h"
+#include "Input/AbilityInputMode.h"
+#include "SettingsViewModel.generated.h"
+
+/**
+ * A view model for modifying and saving settings.
+ */
+UCLASS()
+class CRYSPROJECT_API USettingsViewModel : public UCrysViewModel
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void InitializeViewModel(APlayerController* PlayerController) override;
+	
+	EAbilityInputMode GetAbilityInputMode() const { return AbilityInputMode; }
+	/** Sets the input mode on the Hero. */
+	UFUNCTION(BlueprintCallable, Category = "Viewmodel|Settings", DisplayName = "SetAbilityInputMode")
+	void K2_SetAbilityInputMode(const EAbilityInputMode NewValue);
+	
+protected:
+	UFUNCTION()
+	void SetAbilityInputMode(const EAbilityInputMode NewValue);
+	
+private:
+	UPROPERTY()
+	APlayerController* OwnerPC;
+
+	UPROPERTY(BlueprintReadOnly, FieldNotify, Getter, meta = (AllowPrivateAccess = true))
+	EAbilityInputMode AbilityInputMode;
+};
