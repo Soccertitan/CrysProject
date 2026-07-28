0d28143da06fb60fc8919b21ce2808f7be066013
Soccertitan <tristan.nowacki@outlook.com>
2026-07-28 15:32:06 -0700

    Removed JobGameplayEffectRequirements Class

--- Source/CrysProject/Private/JobSystem/EffectRequirements/JobGameplayEffectRequirement.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Soccertitan 2026
-
-
-#include "JobSystem/EffectRequirements/JobGameplayEffectRequirement.h"
-
-bool UJobGameplayEffectRequirement::ShouldGrantEffect_Implementation(const UJobSystemComponent* JobSystemComponent) const
-{
-	return true;
-}
--- Source/CrysProject/Private/JobSystem/EffectRequirements/JobGameplayEffectRequirement_Level.cpp
+++
@@ -1,4 +0,0 @@
-// Copyright Soccertitan 2026
-
-
-#include "JobSystem/EffectRequirements/JobGameplayEffectRequirement_Level.h"
